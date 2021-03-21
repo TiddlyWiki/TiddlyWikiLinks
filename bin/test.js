2fddfb82ef8c7ea7320d9a5fdf66d8833f221721
jeremy@jermolene.com <jeremy@jermolene.com>
2021-03-21 11:18:07 +0000

    Add a test script to validate sites.json
    The idea is that we'll run this script on PRs to help contributors.

---
+++ bin/test.js
@@ -0,0 +1,28 @@
+/*
+Run tests on sites/sites.json
+*/
+
+const normalizeUrl = require('normalize-url'),
+    assert = require('assert').strict;
+
+function runTests() {
+    const jsonSites = require("../sites/sites.json");
+    assert(Array.isArray(jsonSites),"JSON object should be an array");
+    for(const site of jsonSites) {
+        assert(Object.keys(site).sort().join(",") =="name,url","Each array entry should be an object {name:,url:}");
+        assert(typeof site.name === "string","Name must be a string");
+        assert(site.name.length >= 3 && site.name.length <= 32,"Name must be a string of between 3 and 32 characters");
+        assert(/^[a-z][a-z0-9_\-]*$/mg.test(site.name),"Name must start with a lowercase letter and only contain lower case letters, digits, dashes and underscores")
+        assert(typeof site.url === "string","URL must be a string");
+        const normalizedUrl = normalizeUrl(site.url).toString();
+        assert(normalizedUrl.startsWith("http://") || normalizedUrl.startsWith("https://"),"URL must be a valid http or https URL");
+    }
+}
+
+try {
+    runTests();
+} catch(e) {
+    console.error("Error",e)
+    process.exit(1);
+}
+process.exit(0);
