0a69960b2c94774ce63f48205530eedcba7085e1
Jeremy Ruston <jeremy@jermolene.com>
2021-03-21 17:52:25 +0000

    Tests: Check usernames and URLs are unique

--- bin/test.js
+++ bin/test.js
@@ -6,16 +6,22 @@ const normalizeUrl = require('normalize-url'),
     assert = require('assert').strict;
 
 function runTests() {
-    const jsonSites = require("../sites/sites.json");
+    const jsonSites = require("../sites/sites.json"),
+		usernames = {},
+		urls = {};
     assert(Array.isArray(jsonSites),"JSON object should be an array");
     for(const site of jsonSites) {
         assert(Object.keys(site).sort().join(",") =="name,url","Each array entry should be an object {name:,url:}");
         assert(typeof site.name === "string","Name must be a string");
         assert(site.name.length >= 3 && site.name.length <= 32,"Name must be a string of between 3 and 32 characters");
         assert(/^[a-z][a-z0-9_\-]*$/mg.test(site.name),"Name must start with a lowercase letter and only contain lower case letters, digits, dashes and underscores")
+		assert(!(site.name in usernames),"usernames must be unique");
+		usernames[site.name] = true;
         assert(typeof site.url === "string","URL must be a string");
         const normalizedUrl = normalizeUrl(site.url).toString();
         assert(normalizedUrl.startsWith("http://") || normalizedUrl.startsWith("https://"),"URL must be a valid http or https URL");
+		assert(!(site.url in urls),"urls must be unique");
+		urls[site.url] = true;
     }
 }
 
